7270f5b9a82aa11926aee1fe8d64ebe52fd305c4
kokamate <koka.mate.365@dszcbaross.edu.hu>
2025-03-24 08:03:58 +0100

    valami

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const btnLogin = document.getElementsByClassName('login')[0];
+const btnReg = document.getElementsByClassName('reg')[0];
+
+btnLogin.addEventListener('click', () => {
+    window.location.href = '../html/login.html';
+});
+
+btnReg.addEventListener('click', () => {
+    window.location.href = '../html/registration.html';
+});
+
